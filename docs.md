6ec5bb094ec640f725819588f398840540cb60c6
hosung cho <chspower1@gmail.com>
2023-02-22 18:24:39 +0900

    feat: resolvers 작업중

--- docs.md
+++ docs.md
@@ -8,3 +8,15 @@ Schema Definition Language
 
 GraphQL에 내장되어 있는 type,
 String, Int, Float, Boolean, ID가 있음.
+
+### Query
+
+rest api에서 get이라고 보면 될 듯
+
+### Mutation
+
+rest api에서 post,delete,put이라고 보면 될 듯
+
+### resolvers
+
+resolvers의 인자는 첫번째로 root,두번째부터 원하는 인자가 들어옴
--- server.js
+++ server.js
@@ -1,25 +1,55 @@
 import { ApolloServer, gql } from "apollo-server";
 
+const tweets = [
+  { id: "1", text: "first" },
+  { id: "2", text: "second" },
+];
 const typeDefs = gql`
-  type User {
-    id: ID
-    name: String
-  }
   type Tweet {
-    id: ID
-    text: String
-    author: User
+    id: ID!
+    text: String!
   }
   type Query {
-    allTweets: [Tweet]
-    tweet(id: ID): Tweet
+    allTweets: [Tweet!]
+    tweet(id: ID): Tweet!
   }
   type Mutation {
-    createTweet(text: String, userId: ID): Tweet
-    deleteTweet(id: ID): Boolean
+    createTweet(text: String!): Boolean!
+    deleteTweet(id: ID!): Boolean!
+    updateTweet(id: ID!, newText: String!): Boolean!
   }
 `;
-const server = new ApolloServer({ typeDefs });
+
+const resolvers = {
+  Query: {
+    allTweets() {
+      return tweets;
+    },
+    tweet(_, { id }) {
+      return tweets.find((tweet) => tweet.id === id);
+    },
+  },
+  Mutation: {
+    createTweet(text) {
+      const newItem = { id: String(tweets.length + 1), text };
+      tweets.push(newItem);
+      return true;
+    },
+    deleteTweet(id) {
+      const targetIdx = tweets.findIndex((tweet) => tweet.id === id);
+      if (targetIdx === 0 || targetIdx) {
+        tweets.splice(1, targetIdx + 1);
+        return true;
+      } else return false;
+    },
+    updateTweet(id, newText) {
+      const targetIdx = tweets.findIndex((tweet) => tweet.id === id);
+      tweets[targetIdx] = { ...tweets[targetIdx], text: newText };
+    },
+  },
+};
+
+const server = new ApolloServer({ typeDefs, resolvers });
 
 server.listen().then(({ url }) => {
   console.log(url);
